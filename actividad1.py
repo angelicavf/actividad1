fdb01973bb9121c044dd3b149c3905b0cf2228f6
angelicavf <89312297+angelicavf@users.noreply.github.com>
2022-04-02 20:40:02 -0300

    Entrega final

--- actividad1.py
+++ actividad1.py
@@ -1,3 +1,5 @@
+from tabulate import tabulate
+import numpy as np
 
 lista_producto = [[1, "candado", 4000], [2, "tornillo", 1500], [
     3, "martillo", 6000], [4, "huincha", 2500], [5, "alicate", 6000]]
@@ -7,68 +9,74 @@ comunas = [[1, "estacion Central", 2000], [2, "Providencia", 2500],
 
 totalFinal = 0
 subtotal = 0
-opcion = 0
 cantidad = 0
 fin = 1
 despacho = 0
 tenviocompra = 0
-nombrecomuna = ""
-opcion_comuna = 0
-z = 0
+aux = ""
+factura = []
 
 # mostrando la lista de productos
 
-print("{:<8} {:<15} {:<10}".format('id', 'producto', 'precio'))
+print("Bienvenido a Ferreteria AYV, a continuacion se despliega la lista de productos disponibles: ")
 
-for v in lista_producto:
-    id, producto, precio = v
-    print("{:<8} {:<15} {:<10}".format(id, producto, precio))
-
+print(tabulate(lista_producto, headers=["id", "Producto", "Precio"]))
 
 # selaccionando la opcion del usuario
 
 while fin == 1:
+    opcion = 100
+    while opcion > len(lista_producto):
+        opcion = int(input("ingrese el id de producto a comprar: "))
+        if opcion > len(lista_producto):
+            print("¡Id no válido!")
 
-    opcion = int(input("ingrese el id de producto a comprar: "))
+    aux = lista_producto[opcion-1][1]
     cantidad = int(
-        input("ingrese la cantidad de productos que desea comprar: "))
+        input("ingrese la cantidad de productos que desea comprar de {}: ".format(aux)))
+
 
 # calculando el valo de un porduto seleccionado, con la cantidad indicada
 
-    for x in range(len(lista_producto)-1):
-        if lista_producto[x][0] == opcion:
-            subtotal = lista_producto[x][2] * cantidad
-            nombre = str(lista_producto[x][1])
-            print("El subtotal a pagar por ", cantidad,
-                  " ", nombre, " es: ", subtotal)
+    subtotal = lista_producto[opcion-1][2] * cantidad
+    nombre = str(lista_producto[opcion-1][1])
+    print("El subtotal a pagar por ", cantidad,
+          " ", nombre, " es: ", subtotal)
 
     totalFinal = subtotal+totalFinal
     fin = int(
         input("si desea finalizar la compra ingrese 0 o 1 para continuar comprando :"))
 
+    factura.append([aux, subtotal])
+
+
+factura.append(['Total Compra', totalFinal])
 print("El total de su compra es: ", totalFinal)
+
 
 # selecciona si quiere despacho
 
-despacho = int(input(
-    "si desea contratar despacho ingrese 1 en caso contrario 0 para finalizar: "))
+fin = int(input(
+    "Si desea contratar despacho ingrese 1 en caso contrario 0 para finalizar compra: "))
 
-if(despacho == 1):
+if(fin == 1):
 
-    print("{:<5} {:<20} {:<10}".format('id', 'Comuna', 'envio'))
+    print(tabulate(comunas, headers=["id", "comuna", "Envio"]))
 
-    for v in comunas:
-        id, comuna, envio = v
-        print("{:<5} {:<20} {:<10}".format(id, comuna, envio))
+    opcion = 100
 
-    opcion_comuna = int(input("Indique el numero de su comuna: "))
+    while opcion > len(comunas)+1:
+        opcion = int(input("Indique el numero de su comuna: "))
+        if opcion > len(comunas):
+            print("¡Comuna no valida!")
 
-    for z in range(len(comunas)-1):
-        if comunas[z][0] == opcion_comuna:
-            tenviocompra = comunas[z][2] + totalFinal
-            nombrecomuna = str(comunas[z][1])
+    tenviocompra = comunas[opcion-1][2] + totalFinal
+    aux = str(comunas[opcion-1][1])
 
     print("El total de su compra con envio es: ", tenviocompra,
-          " con despacho a la comuna de ", nombrecomuna)
+          " con despacho a la comuna de ", aux)
+    factura.append(['Total Compra + envío', tenviocompra])
+    print(tabulate(factura, headers=["Detalle", "Monto"]))
+
 else:
     print("El total de su compra sin despacho es: ", totalFinal)
